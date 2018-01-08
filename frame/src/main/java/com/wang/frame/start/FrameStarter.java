c39fb30e39e05c6d8b10b1bef1d03637abf0b5b3
wangju <wang.whu.smile@gmail.com>
2018-01-08 20:36:47 +0800

    delete file

--- frame/src/main/java/com/wang/frame/start/FrameStarter.java
+++ frame/src/main/java/com/wang/frame/start/FrameStarter.java
@@ -3,7 +3,6 @@ package com.wang.frame.start;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.wang.frame.bean.ProviderBean;
-import com.wang.frame.util.SpringContextUtil;
 
 public class FrameStarter {
 
@@ -11,14 +10,6 @@ public class FrameStarter {
 		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
 				"com.wang.frame");
 		applicationContext.start();
-		ProviderBean<?> demo = SpringContextUtil.getBean("vp_DemoProviderService");
-		System.out.println(demo.getId());
-		System.out.println(demo.getService().getName());
-		System.out.println(demo.getVersion());
-		System.out.println(demo.getProtocol());
-
-		ProxyTest proxyTest = SpringContextUtil.getBean("proxyTest");
-		proxyTest.test();
 
 		applicationContext.close();
 	}
